60ca0a42d36aabe5f27926ae581294f204dc7cdf
Sonia Mammone <sonia.mammone95@gmail.com>
2024-06-16 20:41:32 +0200

    refactoring del codice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,27 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { RegisterComponent } from './register/register.component';
-<<<<<<< HEAD
 import { MenuBeverageComponent } from './menu-beverage/menu-beverage.component';
-=======
 import { FilmsComponent } from './films/films.component';
-<<<<<<< HEAD
->>>>>>> Sonia
-=======
 import { LoginComponent } from './login/login.component';
->>>>>>> origin/Sonia
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
     { path: "menu", component: MenuComponent },
     { path: "register", component: RegisterComponent },
-<<<<<<< HEAD
     { path: "menuBeverage", component: MenuBeverageComponent },
-=======
     { path: "films", component: FilmsComponent },
-<<<<<<< HEAD
->>>>>>> Sonia
-=======
     { path: "login", component: LoginComponent },
->>>>>>> origin/Sonia
 ];
