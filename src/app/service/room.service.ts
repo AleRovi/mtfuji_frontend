3a9ddecc8f72255a4d48ecf6091d42eb2873a177
Sonia Mammone <sonia.mammone95@gmail.com>
2024-06-19 22:08:22 +0200

    beautify del codice

--- src/app/service/room.service.ts
+++ src/app/service/room.service.ts
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-=======
 // room.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
->>>>>>> christian_soru
 import { Observable } from 'rxjs';
 import { Room } from '../model/room';
 
@@ -14,16 +9,6 @@ import { Room } from '../model/room';
 })
 export class RoomService {
 
-<<<<<<< HEAD
-  private apiUrl = 'http://localhost:8080/room';
-
-  constructor(private http : HttpClient) { }
-
-  getAllRooms() : Observable<Room> {
-    return this.http.get<Room>(this.apiUrl);
-  }
-}
-=======
   private apiUrl = 'http://localhost:8080/rooms'; // URL del tuo backend
 
   constructor(private http: HttpClient) { }
@@ -33,4 +18,3 @@ export class RoomService {
   }
 }
 
->>>>>>> christian_soru
